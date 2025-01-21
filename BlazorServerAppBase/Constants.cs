f7de2fd709f7c1c35d020b7396ad1a581b5d005e
ys.cho <ys.cho@fstc.co.kr>
2025-01-21 19:08:31 +0900

    First Commit

---
+++ BlazorServerAppBase/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorServerAppBase
+{
+    public class Constants
+    {
+        public const string BaseVersion = "1.0.0";
+        public const string BaseTitle = "FST Digital System";
+    }
+}
---
+++ BlazorServerAppBase/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BlazorServerAppBase.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerAppBase.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+        }
+    }
+}
